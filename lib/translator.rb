dd9da4b4de18f650ab7c375d6153b34ee8b90348
Marco Frisina <marco@frisina.xyz>
2019-07-03 20:40:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_english_meaning(file_path, japanese_emoticon)
   if library["get_emoticon"].include?(japanese_emoticon)
     library["get_emoticon"][english_emoticon]
   else
-    "Sorry, that emoticon was not found"
+    "Sad"
   end
   # code goes here
 end

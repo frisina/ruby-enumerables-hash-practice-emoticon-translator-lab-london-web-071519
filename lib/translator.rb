b3b05e8811b4df2250f402e4ce915ce41c5ef79c
Marco Frisina <marco@frisina.xyz>
2019-07-03 20:33:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ end
 
 def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   # code goes here
-  library = load_library(path)
+  library = load_library(file_path)
   if library["get_emoticon"].include?(english_emoticon)
     library["get_emotion"][english_emoticon]
   else
